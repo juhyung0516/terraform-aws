d0692177350b687f042cc3aa796ca70de819219f
Juhyung Hong <ainu0516@gmail.com>
2024-11-06 15:12:44 +0900

    add folder structure + vpc test3

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,3 +1,4 @@
+# VPC 설정
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -7,4 +8,61 @@ resource "aws_vpc" "main" {
   }
 }
 
-# 추가로 서브넷, 게이트웨이 등 필요한 리소스 정의
+# 퍼블릭 서브넷 생성 (웹 서버용)
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.vpc_name}-public-a"
+  }
+}
+
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.aws_region}b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.vpc_name}-public-b"
+  }
+}
+
+# 애플리케이션 서버용 프라이빗 서브넷 생성
+resource "aws_subnet" "app_subnet_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "${var.aws_region}a"
+  tags = {
+    Name = "${var.vpc_name}-app-a"
+  }
+}
+
+resource "aws_subnet" "app_subnet_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "${var.aws_region}b"
+  tags = {
+    Name = "${var.vpc_name}-app-b"
+  }
+}
+
+# 데이터베이스 서버용 프라이빗 서브넷 생성
+resource "aws_subnet" "db_subnet_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "${var.aws_region}a"
+  tags = {
+    Name = "${var.vpc_name}-db-a"
+  }
+}
+
+resource "aws_subnet" "db_subnet_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "${var.aws_region}b"
+  tags = {
+    Name = "${var.vpc_name}-db-b"
+  }
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,4 +1,11 @@
 variable "vpc_name" {
   description = "The name of the VPC"
   type        = string
+  default     = "test3"
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "ap-northeast-2"
 }
