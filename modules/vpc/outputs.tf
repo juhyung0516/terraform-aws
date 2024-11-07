3ee64dcacf80ba6a3f844d67a43c69f51ad3ec44
Juhyung Hong <ainu0516@gmail.com>
2024-11-07 19:18:12 +0900

    hotfix

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -6,7 +6,7 @@ output "vpc_id" {
 
 output "private_subnet_ids" {
   description = "List of private subnet IDs for the RDS and ASG instances"
-  value       = aws_subnet.private[*].id
+  value       = aws_subnet.private_app[*].id
 }
 
 output "private_db_subnet_ids" {
