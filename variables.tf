f1db4aabcaa120485401c226d289d71eb5776114
Juhyung Hong <ainu0516@gmail.com>
2024-11-07 15:51:56 +0900

    .

--- variables.tf
+++ variables.tf
@@ -2,11 +2,13 @@
 variable "aws_region" {
   description = "AWS Region"
   type        = string
+  default = "ap-northeast-2"
 }
 
 variable "vpc_name" {
   description = "The name of the VPC"
   type        = string
+  default     = "three-tier-vpc"
 }
 
 # VPC CIDR
